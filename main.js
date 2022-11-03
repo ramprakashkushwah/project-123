a98af597f28e871b43f404769bf5cf9d0405e759
ramprakashkushwah <102748887+ramprakashkushwah@users.noreply.github.com>
2022-11-03 19:58:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300)
+    video.hide();
+
+ 
+
+}
+function draw(){
+   image(video,0,0,300,300); 
+}
+function take_spanshot(){
+    save('MyFilterImage.png');
+}
+function preload(){
+
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
